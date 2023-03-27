1380fda17774710606c16b18abd314c46797d658
marcoantn020 <marcoantn020@gmail.com>
2023-03-27 19:47:00 -0300

    first commit

---
+++ src/data/word.js
@@ -0,0 +1,8 @@
+export const wordList = {
+  carro: ['Motor','Porta','Pneu', 'Antena'],
+  fruta: ['Banana','Maçã','Pêra','Mamão','Laranja'],
+  corpo: ['Braço','Perna','Cérebro','Pescoço','Olhos'],
+  computador: ['Mouse','Teclado','Monitor','Gabinete'],
+  programacao: ['Linguagem','Framework','Javascript','React'],
+  alimento: ['Arroz','Feijão','Carne','Leite','Ovo']
+}
